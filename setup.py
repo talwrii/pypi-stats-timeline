48fb81cb257fdb8a0b6ecb3fee17f3886301bda3
Tal Wrii <talwrii@gmail.com>
2025-04-27 01:14:58 +0200

    set version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import distutils.core
 
 setuptools.setup(
     name='pypi-stats-timeline',
-    version="1.0.1",
+    version="1.0.0",
     author='@readwithai',
     long_description_content_type='text/markdown',
     author_email='talwrii@gmail.com',
