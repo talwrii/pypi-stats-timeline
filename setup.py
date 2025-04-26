614843d278249583cb257aac614136a4021d1705
Tal Wrii <talwrii@gmail.com>
2025-04-27 01:14:39 +0200

    add description

--- setup.py
+++ setup.py
@@ -4,19 +4,16 @@ import distutils.core
 setuptools.setup(
     name='pypi-stats-timeline',
     version="1.0.1",
-    author='Author',
+    author='@readwithai',
     long_description_content_type='text/markdown',
-    author_email='Email',
-    description='',
-    license='BSD3',
-    keywords='',
-    url='',
+    author_email='talwrii@gmail.com',
+    description="Fetch, maintain and query a timeline of downloads from Python's PyPI Package indexes used by pip",
+    license='MIT',
+    keywords='python,cli,PyPI,timeline',
+    url='https://github.com/talwrii/pypi-stats-timeline',
     packages=["pypi_stats_timeline"],
     long_description=open('README.md').read(),
     entry_points={
         'console_scripts': ['pypi-stats-timeline=pypi_stats_timeline.main:main']
     },
-    classifiers=[
-    ],
-    test_suite='nose.collector'
 )
